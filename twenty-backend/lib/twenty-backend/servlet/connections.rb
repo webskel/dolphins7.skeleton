b75e14ff37c95e783bafc1e907acbd269cdc54ce
0x1eef <0x1eef@protonmail.com>
2023-12-11 08:57:42 -0300

    backend: res_body -> body

--- twenty-backend/lib/twenty-backend/servlet/connections.rb
+++ twenty-backend/lib/twenty-backend/servlet/connections.rb
@@ -1,12 +1,12 @@
 class Twenty::Servlet::Connections < Twenty::Servlet
   def do_GET(req, res)
     write res,
-          [200, {'content-type' => 'application/json'}, res_body]
+          [200, {'content-type' => 'application/json'}, body]
   end
 
   private
 
-  def res_body
+  def body
     JSON.dump database.connections.map { Ryo.table_of(_1,recursive: true) }
   end
 end
