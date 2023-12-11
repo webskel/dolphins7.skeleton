bda4f9fa83aef66ba560f0eda48d95cbd0bc93da
0x1eef <0x1eef@protonmail.com>
2023-12-11 08:49:05 -0300

    backend: add "res_body" method to Connections

--- twenty-backend/lib/twenty-backend/servlet/connections.rb
+++ twenty-backend/lib/twenty-backend/servlet/connections.rb
@@ -1,7 +1,12 @@
 class Twenty::Servlet::Connections < Twenty::Servlet
   def do_GET(req, res)
-    res_body = JSON.dump database.connections.map { Ryo.table_of(_1,recursive: true) }
     write res,
           [200, {'content-type' => 'application/json'}, res_body]
+  end
+
+  private
+
+  def res_body
+    JSON.dump database.connections.map { Ryo.table_of(_1,recursive: true) }
   end
 end
