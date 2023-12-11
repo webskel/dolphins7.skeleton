e5c148532aaf3c5af47997860c2f78c6f0e87e7a
0x1eef <0x1eef@protonmail.com>
2023-12-11 12:49:48 -0300

    Render connections

--- twenty-frontend/src/js/components/Connections.tsx
+++ twenty-frontend/src/js/components/Connections.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+type Connection = {
+  name: string;
+  path: string;
+}
 
 export function Connections() {
+  const [connections, setConnections] = useState<Connection[]>([]);
+  const items = connections.map((conn, i) => {
+    return (<li key={i}>{conn.name}</li>);
+  })
+
+  useEffect(() => {
+    fetch("/connections.json")
+    .then((res) => res.json())
+    .then((conns: Connection[]) => setConnections(conns))
+  }, []);
+
+
   return (
     <>
-      <span className="connections">Connections</span>
+      <span>Connections</span>
+      <ul className="connections">
+        {items}
+      </ul>
     </>
   )
 }
