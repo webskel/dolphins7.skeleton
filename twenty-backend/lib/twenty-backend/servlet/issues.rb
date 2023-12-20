3c14c7f02f0daead0647885775b6ec554be7c97d
0x1eef <0x1eef@protonmail.com>
2023-12-20 19:37:59 -0300

    backend: implement Issues#do_POST

--- twenty-backend/lib/twenty-backend/servlet/issues.rb
+++ twenty-backend/lib/twenty-backend/servlet/issues.rb
@@ -1,4 +1,14 @@
 class Twenty::Servlet::Issues < Twenty::Servlet
   def do_POST(req, res)
+    Twenty::Issue.new(
+      JSON.parse(req.body)
+    ).save!
+    write res,
+          {"content-type" => "application/json"},
+          {"ok": true, errors: []}.to_json
+  rescue
+    write res,
+          {"content-type" => "application/json"},
+          {"ok": false, errors: [$!.class]}.to_json
   end
 end
