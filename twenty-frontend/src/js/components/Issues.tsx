b2f0404f3678108ef49bc3b15a576e9380cc2c00
0x1eef <0x1eef@protonmail.com>
2023-12-21 03:16:02 -0300

    frontend: link to /issues/new from /issues/

--- twenty-frontend/src/js/components/Issues.tsx
+++ twenty-frontend/src/js/components/Issues.tsx
@@ -4,9 +4,16 @@ import { useIssues } from "/hooks/useIssues";
 export function Issues() {
   const [issues] = useIssues();
   return (
-    <>
-      <span>Issues</span>
-      <ul className="connections">
+    <div className="pure-u-1-1 issue-index">
+      <div className="pure-u-2-5">
+        <span>Issues</span>
+      </div>
+      <div className="pure-u-3-5 new-issue-btn">
+        <button className="pure-button pure-button-primary">
+          <a href={`/issues/new`}>New Issue</a>
+        </button>
+      </div>
+      <ul className="pure-u-5-5">
         {issues.map((issue, key) => {
           return (
             <li key={key}>
@@ -15,6 +22,6 @@ export function Issues() {
           );
         })}
       </ul>
-    </>
+    </div>
   );
 }
