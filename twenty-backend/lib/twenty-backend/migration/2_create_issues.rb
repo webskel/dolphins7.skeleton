061af3b8d4ce9a9181e9a8cc735b572321c95ba5
0x1eef <0x1eef@protonmail.com>
2023-12-20 22:56:01 -0300

    backend: state defaults to "open"

--- twenty-backend/lib/twenty-backend/migration/2_create_issues.rb
+++ twenty-backend/lib/twenty-backend/migration/2_create_issues.rb
@@ -3,7 +3,7 @@ class CreateIssues < ActiveRecord::Migration[7.1]
     create_table(:issues) do |t|
       t.string :title, null: false
       t.text :content, null: false
-      t.string :state, null: false
+      t.string :state, null: false, default: "open"
       t.belongs_to :connection, null: false
       t.timestamps
     end
