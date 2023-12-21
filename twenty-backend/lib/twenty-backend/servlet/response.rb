b8afeccc39664f3d4a65ea13d87fa5995cd1da8c
0x1eef <0x1eef@protonmail.com>
2023-12-21 02:24:06 -0300

    backend: add Twenty::Servlet::Response docs

--- twenty-backend/lib/twenty-backend/servlet/response.rb
+++ twenty-backend/lib/twenty-backend/servlet/response.rb
@@ -1,28 +1,49 @@
 class Twenty::Servlet::Response
-  attr_reader :res
-
+  ##
+  # @param [WEBrick::HTTPResponse] res
+  #  An instance of WEBrick::HTTPResponse.
+  # @return [Twenty::Servlet::Response]
+  #  Returns an instance of Twenty::Servlet::Response.
   def initialize(res)
     @res = res
     set_headers({"content-type" => "application/json"})
   end
 
+  ##
+  # Sets the response status.
+  # @param [Integer] status
+  #  A status code.
+  # @return [Twenty::Servlet::Response]
+  #  Returns self.
   def set_status(status)
     res.status = status
     self
   end
 
+  ##
+  # Sets the response body.
+  # @param [#to_json] body
+  #  The response body.
+  # @return [Twenty::Servlet::Response]
+  #  Returns self.
   def set_body(body)
     res.body = default_body_for(res.status).merge(body).to_json
     self
   end
 
+  ##
+  # Sets the response headers.
+  # @param [#each] headers
+  #  The response headers.
+  # @return [Twenty::Servlet::Response]
+  #  Returns self.
   def set_headers(headers)
     headers.each { res[_1] = _2 }
     self
   end
 
   private
-
+  attr_reader :res
   def default_body_for(status)
     case status
     when 200
