2497a340be29b9360f8d72201245e85cbf1bf394
0x1eef <0x1eef@protonmail.com>
2023-12-23 14:25:17 -0300

    frontend: fetch task in edit.tsx

--- twenty-frontend/src/js/main/task/edit.tsx
+++ twenty-frontend/src/js/main/task/edit.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Task } from "/components/Task";
+import { Task as Component } from "/components/Task";
+import { Task } from "/types/schema";
 
 (function () {
-  const root = document.querySelector(".react-mount.edit-task")!;
-  ReactDOM.createRoot(root).render(<Task/>);
+  const params = Object.fromEntries(
+    location.hash
+      .substring(1, location.hash.length)
+      .split(",")
+      .map(e => e.split("=")),
+  );
+  fetch(`/servlet/tasks/${params.id}`)
+    .then(res => res.json())
+    .then(({task}: {task: Task}) => {
+      const root = document.querySelector(".react-mount.edit-task")!;
+      ReactDOM.createRoot(root).render(<Component task={task} />);
+    });
 })();
