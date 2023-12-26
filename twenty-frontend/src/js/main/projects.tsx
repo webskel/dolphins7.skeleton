c330c8476220009aa9c4b829d5ecd606fbad6a85
0x1eef <0x1eef@protonmail.com>
2023-12-26 01:19:14 -0300

    frontend: fix broken src/js/main/projects.tsx

--- twenty-frontend/src/js/main/projects.tsx
+++ twenty-frontend/src/js/main/projects.tsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Projects } from "/components/Projects";
 
 (function () {
-  const root = document.getElementById(".react-mount.projects")!;
+  const root = document.querySelector(".react-mount.projects")!;
   ReactDOM.createRoot(root).render(<Projects />);
 })();
