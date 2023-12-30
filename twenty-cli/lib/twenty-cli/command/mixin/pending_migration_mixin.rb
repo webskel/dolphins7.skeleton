52297a255656c5e437237dd06cd365f69c30915b
0x1eef <0x1eef@protonmail.com>
2023-12-30 00:17:16 -0300

    cli: add 'pending_migrations?' shortcut

--- twenty-cli/lib/twenty-cli/command/mixin/pending_migration_mixin.rb
+++ twenty-cli/lib/twenty-cli/command/mixin/pending_migration_mixin.rb
@@ -1,11 +1,16 @@
 module Twenty::Command::PendingMigrationMixin
   def run_command(...)
-    if Twenty::Migration.pending_migrations?
+    if pending_migrations?
       warn "There are pending migrations.\n" \
            "Run \"twenty migrate\" first.\n"
       exit(1)
     else
       super(...)
     end
+  end
+  private
+
+  def pending_migrations?
+    Twenty::Migration.pending_migrations?
   end
 end
